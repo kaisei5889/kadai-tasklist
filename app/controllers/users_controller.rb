d703f55ba75608d12b3dc4a3422d49cd8ffbf6ee
ryo.hiwada <ryo.hiwada@g.softbank.co.jp>
2021-02-05 11:36:25 +0000

    remove extra file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:show]
   
   def new
     @user = User.new
@@ -15,11 +14,6 @@ class UsersController < ApplicationController
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
-  end
-  
-  def show
-    @user = User.find(params[:id])
-    @tasks = @user.tasks.order(id: :desc).page(params[:page])
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create]
   
   resources :tasks
 end
